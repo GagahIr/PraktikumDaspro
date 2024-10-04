a172e382b2da0ef9678919b1dbd22ce415adcdd0
GagahIr <gagahirlangsa30@gmail.com>
2024-10-04 07:13:02 +0700

    Praktikum06

---
+++ Praktikum06/Pemilihan2Percoban1_10.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class Pemilihan2Percoban1_10 {
+    public static void main(String[] args) {
+
+        Scanner input10 = new Scanner(System.in);
+    
+        int tahun;
+    
+        System.out.println("Masukkan Tahun");
+        tahun = input10.nextInt();
+    
+        if (tahun % 4 == 0) {
+            if ((tahun % 100) != 0) {
+                System.out.println("Tahun Kabisat");
+            }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+    }
+    }
+}
